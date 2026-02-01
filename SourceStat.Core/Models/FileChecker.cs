7972b47834acd05af3f968d1d46205dd395030b7
sergxlove <sergxlove@mail.ru>
2026-02-01 18:45:13 +0600

    add models in core

---
+++ SourceStat.Core/Models/FileChecker.cs
@@ -0,0 +1,29 @@
+﻿namespace SourceStat.Core.Models
+{
+    public class FileChecker
+    {
+        public static long GetCountFiles(string directory, string extensions)
+        {
+            long fileCount = 0;
+            foreach(string file in Directory.EnumerateFiles(directory, extensions, 
+                SearchOption.AllDirectories))
+            {
+                fileCount++;
+            }
+            return fileCount;
+        }
+
+        public static long GetCountLineInFiles(string directory, string extensions)
+        {
+            long lineCount = 0;
+            string[] lines;
+            foreach (string file in Directory.EnumerateFiles(directory, extensions, 
+                SearchOption.AllDirectories))
+            {
+                lines = File.ReadAllLines(file);
+                lineCount += lines.Length;
+            }
+            return lineCount;
+        }
+    }
+}
