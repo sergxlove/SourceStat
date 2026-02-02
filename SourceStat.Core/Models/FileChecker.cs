abac35b0353fd619c5fa57aec3175d2cc4ca906d
sergxlove <sergxlove@mail.ru>
2026-02-02 15:25:23 +0600

    add options file checker, add graphical app projects

--- SourceStat.Core/Models/FileChecker.cs
+++ SourceStat.Core/Models/FileChecker.cs
@@ -2,28 +2,53 @@
 {
     public class FileChecker
     {
-        public static long GetCountFiles(string directory, string extensions)
+        public static long GetCountFiles(string directory, FileCheckerOptions options)
         {
             long fileCount = 0;
-            foreach(string file in Directory.EnumerateFiles(directory, extensions, 
-                SearchOption.AllDirectories))
+            foreach(string extensions in options.SelectExtensions)
             {
-                fileCount++;
+                foreach(string file in Directory.EnumerateFiles(directory, extensions, 
+                    SearchOption.AllDirectories))
+                {
+                    if(!IsInIgnoredDir(file, options)) fileCount++;
+                }
             }
             return fileCount;
         }
 
-        public static long GetCountLineInFiles(string directory, string extensions)
+        public static long GetCountLineInFiles(string directory, FileCheckerOptions options)
         {
             long lineCount = 0;
             string[] lines;
-            foreach (string file in Directory.EnumerateFiles(directory, extensions, 
-                SearchOption.AllDirectories))
+            foreach (string extensions in options.SelectExtensions)
             {
-                lines = File.ReadAllLines(file);
-                lineCount += lines.Length;
+                foreach (string file in Directory.EnumerateFiles(directory, extensions, 
+                    SearchOption.AllDirectories))
+                {
+                    if (!IsInIgnoredDir(file, options))
+                    {
+                        lines = File.ReadAllLines(file);
+                        lineCount += lines.Length;
+                    }
+                }
             }
             return lineCount;
+        }
+
+        private static bool IsInIgnoredDir(string filePath, FileCheckerOptions options)
+        {
+            string? directory = Path.GetDirectoryName(filePath);
+            if (string.IsNullOrEmpty(directory))
+                return false;
+            string[] folders = directory.Split(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar);
+            foreach (string folder in folders)
+            {
+                if(options.IgnoreDirectories.Contains(folder))
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
