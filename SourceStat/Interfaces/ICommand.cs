1f6bffa00b3401c6703641d6760d1d0c446a0055
sergxlove <sergxlove@mail.ru>
2026-02-04 22:10:42 +0600

    add core execute console commands

---
+++ SourceStat/Interfaces/ICommand.cs
@@ -0,0 +1,9 @@
+﻿namespace SourceStat.Interfaces
+{
+    public interface ICommand
+    {
+        string Name { get; }
+        string Description { get; }
+        Task Execute(string[] args);
+    }
+}
---
+++ SourceStat/Models/ExecuteCommandCore.cs
@@ -0,0 +1,31 @@
+﻿using SourceStat.Interfaces;
+
+namespace SourceStat.Models
+{
+    public class ExecuteCommandCore
+    {
+        public List<ICommand> Commands { get; } = [];
+
+        public void ExecuteCommand(string command)
+        {
+            string[] parts = command.Split(' ');
+            string cmdName = parts[0];
+            string[] args = parts.Skip(1).ToArray();
+            var cmd = Commands.FirstOrDefault(a => a.Name == cmdName);
+            if (cmd != null)
+            {
+                cmd.Execute(args);
+            }
+        }
+
+        public void Add(ICommand command)
+        {
+            Commands.Add(command);
+        }
+
+        public void AddRange(ICommand[] commands)
+        {
+            Commands.AddRange(commands);
+        }
+    }
+}
